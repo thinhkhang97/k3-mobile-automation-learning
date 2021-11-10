4d1f54360262672a4a37b72732444d39a8279d64
khangtnguyen <thinhkhang97@gmail.com>
2021-11-10 14:12:46 +0700

    Init project with lesson 13

---
+++ src/capabilities/MobileCapabilityTypeEx.java
@@ -0,0 +1,9 @@
+package capabilities;
+
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public interface MobileCapabilityTypeEx extends MobileCapabilityType {
+    String APP_PACKAGE = "appPackage";
+
+    String APP_ACTIVITY = "appActivity";
+}
