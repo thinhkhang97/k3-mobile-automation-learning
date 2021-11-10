52a16ea3e79541db97322e4fe5ae6b1e4c0232c5
khangtnguyen <thinhkhang97@gmail.com>
2021-11-10 15:12:27 +0700

    Lesson 14: using driver to find element

---
+++ src/lesson14/drivers/AndroidDriverFactory.java
@@ -0,0 +1,97 @@
+package lesson14.drivers;
+
+import capabilities.MobileCapabilityTypeEx;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class AndroidDriverFactory {
+
+    private final Long implicitlyWaitTime = 30L;
+
+    private URL url;
+
+    private String platformName = "Android";
+
+    private String udid = "emulator-5554";
+
+    private String automationName = "uiautomator2";
+
+    private String appPackage = "com.wdiodemoapp";
+
+    private String appActivity = "com.wdiodemoapp.MainActivity";
+
+    AndroidDriver<MobileElement> driver;
+
+    DesiredCapabilities desiredCapabilities;
+
+    public AndroidDriverFactory(URL url) {
+        this.url = url;
+        desiredCapabilities = new DesiredCapabilities();
+    }
+
+    public URL getUrl() {
+        return url;
+    }
+
+    public void setUrl(URL url) {
+        this.url = url;
+    }
+
+    public String getPlatformName() {
+        return platformName;
+    }
+
+    public void setPlatformName(String platformName) {
+        this.platformName = platformName;
+    }
+
+    public String getUdid() {
+        return udid;
+    }
+
+    public void setUdid(String udid) {
+        this.udid = udid;
+    }
+
+    public String getAutomationName() {
+        return automationName;
+    }
+
+    public void setAutomationName(String automationName) {
+        this.automationName = automationName;
+    }
+
+    public String getAppPackage() {
+        return appPackage;
+    }
+
+    public void setAppPackage(String appPackage) {
+        this.appPackage = appPackage;
+    }
+
+    public String getAppActivity() {
+        return appActivity;
+    }
+
+    public void setAppActivity(String appActivity) {
+        this.appActivity = appActivity;
+    }
+
+    public io.appium.java_client.android.AndroidDriver<MobileElement> getDriver() {
+        desiredCapabilities.setCapability(MobileCapabilityTypeEx.PLATFORM_NAME, this.platformName);
+        desiredCapabilities.setCapability(MobileCapabilityTypeEx.UDID, this.udid);
+        desiredCapabilities.setCapability(MobileCapabilityTypeEx.AUTOMATION_NAME, this.automationName);
+        desiredCapabilities.setCapability(MobileCapabilityTypeEx.APP_PACKAGE, this.appPackage);
+        desiredCapabilities.setCapability(MobileCapabilityTypeEx.APP_ACTIVITY, this.appActivity);
+        desiredCapabilities.setCapability(MobileCapabilityTypeEx.NEW_COMMAND_TIMEOUT, 120);
+
+        driver = new AndroidDriver<MobileElement>(this.url, desiredCapabilities);
+        driver.manage().timeouts().implicitlyWait(this.implicitlyWaitTime, TimeUnit.SECONDS);
+
+        return driver;
+    }
+}
